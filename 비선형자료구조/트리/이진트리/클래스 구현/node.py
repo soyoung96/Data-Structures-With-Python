bc82adb310e13eb2b9a3e5d51b32fbbe5f14041c
soyoung96 <psy411301@naver.com>
2022-10-21 16:12:34 +0900

    이진트리 순회 구현

---
+++ 비선형자료구조/트리/이진트리/클래스 구현/node.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self,key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.parent = None
+    def __str__(self):
+        return str(self.key)
+    def preorder(self):
+        if(self!=None):
+            print(self.key)
+            if(self.left):
+                self.left.preorder()
+            if(self.right):
+                self.right.preorder()
+
+    def inorder(self):
+        if(self!=None):
+            if(self.left):
+                self.left.inorder()
+            print(self.key)
+            if(self.right):
+                self.right.inorder()
+
+    def postorder(self):
+        if(self!=None):
+            if(self.left):
+                self.left.postorder()
+            if(self.right):
+                self.right.postorder()
+            print(self.key)
+
+a = Node("A")
+b = Node("B")
+c = Node("C")
+d = Node("D")
+g = Node("G")
+i = Node("I")
+h = Node("H")
+f = Node("f")
+
+a.parent = b
+d.parent = b
+b.left = a
+b.right = d
+
+g.right = i
+i.parent=g
+h.parent = i
+i.left = h
+
+b.parent = f
+g.parent = f
+f.left = b
+f.right = g
+
+#이진트리 탐색
+
+print("f.preorder()")
+f.preorder()
+print("f.inorder()")
+f.inorder()
+print("f.postorder()")
+f.postorder()
